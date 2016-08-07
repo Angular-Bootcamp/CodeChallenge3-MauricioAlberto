e15a071a8bcaa1cf212616f4b0bfe33669c9f4da
MauriceStark <maurice222@outlook.com>
2016-08-07 17:31:57 -0500

    Create angular service evolutionService.js

---
+++ app/scripts/services/evolutionService.js
@@ -0,0 +1,30 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('pokedexApp')
+    .service('evolutionService', Service);
+
+  Service.$inject = ['$filter'];
+
+  function Service($filter) {
+    var vm = this;
+
+    vm.getEvolutions = function(pokemons, evolutions) {
+
+      Array.prototype.customIndexOf = function(searchElement) {
+        return this.map(function(value) {
+          // Return: array value (string lowercase)
+          return value.toLowerCase();
+          // searchElement value (string lowercase)
+        }).indexOf(searchElement.toLowerCase());
+      };
+
+      return $filter('filter')(pokemons, function(data){
+        return (evolutions.customIndexOf(data.name) >= 0);
+      });
+    }
+
+  }
+
+})();
